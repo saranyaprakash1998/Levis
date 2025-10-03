8710c993bdf8bf8e5597ababd626a90549c7bd85
saranyaprakash1998 <mailtosaranyavv@gmail.com>
2025-10-03 17:31:29 +0530

    Changed

--- src/Components/FAQS/Faqs.jsx
+++ src/Components/FAQS/Faqs.jsx
@@ -4,15 +4,15 @@ import FAQItem from "./Faqanswe";
 
 export default function Faqs(){
     return(
-        <div class="flex flex-col md:flex-row-2 lg:flex-row justify-around mb-20">
-            <div >
+        <div class="flex flex-col md:flex-row-2 lg:flex-row justify-around mb-20 px-4">
+            <div class="w-full md:w-1/2 p-4 md:p-0">
                 <nav class="flex  gap-2 text-emerald-600 text-sm p-3 mt-20">
                     <DotIcon/>
                     <h1>FAQS</h1>
                 </nav>
                 <h1 class="text-emerald-900 text-[40px] mb-6">Frequently <b>asked</b> questions</h1>
-                <p class="text-gray-600">Explore our Frequently Asked Questions for quick answers on policies, claims, and </p>
-                <p class="text-gray-600">coverage, helping you make informed insurance decisions.</p>
+                <p class="text-gray-600">Explore our Frequently Asked Questions for quick answers on policies, claims, and 
+                coverage, helping you make informed insurance decisions.</p>
 
                 <div class="mt-10">
                     <div class="flex items-center gap-3">
@@ -20,8 +20,8 @@ export default function Faqs(){
                          <h1 class="flex gap-3 py-2 font-bold text-teal-800">20+ Years Of Experience</h1>
                     </div>
                     <div>
-                    <p class="text-gray-600 px-6 mt-3">In the new era of technology we look in the future with certainly and pride,</p>
-                    <p class=" text-gray-600 px-6"> that's why so.</p>
+                    <p class="text-gray-600 px-6 mt-3">In the new era of technology we look in the future with certainly and pride,
+                    that's why so.</p>
                     </div>
                 </div>
                 <div class="mt-10">
@@ -30,8 +30,8 @@ export default function Faqs(){
                         <h1  class="flex gap-3 py-2 font-bold text-teal-800">24/7 Support Available</h1>
                     </div>
                     <div>
-                    <p class="text-gray-600 px-6 mt-3">In the new era of technology we look in the future with certainly and pride,</p>
-                    <p class=" text-gray-600 px-6"> that's why so.</p>
+                    <p class="text-gray-600 px-6 mt-3">In the new era of technology we look in the future with certainly and pride,
+                     that's why so.</p>
                     </div>
 
                 </div>
